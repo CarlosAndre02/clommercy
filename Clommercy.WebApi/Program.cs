77660ad210e5912383379722f0ae5ce139734447
Carlos André <dev.carlosandre@gmail.com>
2025-04-08 00:46:42 -0300

    feat(webapi): add initial server configuration

--- Clommercy.WebApi/Program.cs
+++ Clommercy.WebApi/Program.cs
@@ -1,16 +1,27 @@
+using Clommercy.Core;
+using Clommercy.Persistence;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.ConfigureApplicationApp();
+builder.Services.ConfigurePersistenceApp(builder.Configuration);
+
+builder.Services.AddControllers();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+
+    app.UseSwaggerUi(options =>
+    {
+        options.DocumentPath = "/openapi/v1.json";
+    });
 }
+
+app.MapControllers();
 
 app.UseHttpsRedirection();
 
