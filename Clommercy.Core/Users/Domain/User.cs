4c84cbd3299246a6ebb5221799bcf18b24f3ee93
Carlos André <dev.carlosandre@gmail.com>
2025-04-09 00:14:48 -0300

    feat(core): add user entity

---
+++ Clommercy.Core/Users/Domain/User.cs
@@ -0,0 +1,16 @@
+using Clommercy.Core.Shared.Contracts;
+
+namespace Clommercy.Core.Users.Domain;
+
+public sealed class User : BaseEntity
+{
+    public required string Email { get; set; }
+    public required string Name { get; set; }
+    public required string Password { get; set; }
+
+    public User(string email, string name, string password) {
+        Email = email;
+        Name = name;
+        Password = password;
+    }
+}
